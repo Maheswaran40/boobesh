0d4f34cf3d8ab95abc2637408fd26b66988c2ec5
Maheswaran40 <eswermahes@gmail.com>
2026-07-01 12:55:36 +0530

    search Function created

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -13,9 +13,68 @@ function loginFun(e){
     }
 }
 
-// function fun2(){
-//     document.getElementById("head").style.color="red"
-//     document.getElementById("head").style.backgroundColor="yellow"
-//     document.getElementById("head").innerText="division "
-//     document.getElementById("head").innerText="<h1>division</h1>"
-// }
+// JSON =Javascript object notation 
+
+
+let All_products=[
+    {id:1,name:"iphone s  ",price:20000,image:"../images/iphone.png",brand:"mobile"},
+    {id:2,name:"vivos",price:1000,image:"../images/vivo.png"},
+    {id:3,name:"oppos",price:2000,image:"../images/samsung.png"},
+    {id:4,name:"samsung",price:3000,image:"../images/samsung.png"},
+    {id:4,name:"samsung",price:3000,image:"../images/samsung.png"},
+]
+
+
+
+function showData(){
+    let data=""
+    All_products.map((value,index)=>(
+        data+=`
+        <div class="col-lg-3">
+            <div class="card">
+                <img src="${value.image}" height="200px" alt="">
+                <div class="card-body">
+                    <span>${value.name}</span>
+                    <h5>${value.price}</h5>
+                </div>
+            </div>
+        </div>
+        `
+    ))
+
+document.getElementById("showData").innerHTML=data
+
+}
+showData()
+
+
+function searchFun(e){
+e.preventDefault()
+let searchData=document.getElementById("input").value
+console.log(searchData);
+
+// let output=All_products.filter((value,index)=> value.name.toLowerCase().trim() == searchData.toLowerCase().trim() || value.price == searchData || value.brand == searchData )
+let output=All_products.filter((value,index)=> value.name.toLowerCase().trim().includes(searchData.toLowerCase().trim())  || value.price == searchData || value.brand == searchData )
+
+
+console.log("output",output);
+
+let data=""
+output.map((v,i)=>(
+    data+=`
+      <div class="col-lg-3">
+            <div class="card">
+                <img src="${v.image}" height="200px" alt="">
+                <div class="card-body">
+                    <span>${v.name}</span>
+                    <h5>${v.price}</h5>
+                </div>
+            </div>
+        </div>
+    `
+))
+document.getElementById("searchData").innerHTML=data
+}
+
+
+
